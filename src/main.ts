b04416ef7d5efe187d72c9540a3e7b408d698fa9
Artur Jordão <artur.dev@hotmail.com>
2024-02-26 10:13:03 -0300

    Revert "k"
    This reverts commit ffbb4aaf366769142f6fa0e9481e01997c34bc69.

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-<<<<<<< HEAD
   await app.listen(process.env.PORT ?? 3000);
-=======
-  await app.listen(3000);
->>>>>>> parent of c43e21a (teste)
 }
+
 bootstrap();
