5a8fb02d386af4b375ae330846153c525ceb5d1b
Artur Jordão <artur.dev@hotmail.com>
2024-05-16 19:57:24 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Habilite o CORS com configurações específicas
+  app.enableCors({
+    origin: '*', // Permite todas as origens. Modifique conforme necessário.
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  });
+
   await app.listen(3000);
 }
 bootstrap();
